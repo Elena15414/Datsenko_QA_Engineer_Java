ff5849132b304b1a37598dc493e741f2b87b30d8
Elena Datsenko <elenasheglova15414@gmail.com>
2025-04-30 23:28:35 +0300

    readme

---
+++ src/main/java/ru/datsenko/Main.java
@@ -0,0 +1,24 @@
+package ru.datsenko;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        System.out.println("a+b: " + (a + b));
+        System.out.println("a-b: " + (a - b));
+        System.out.println("a/b: " + (a / b));
+        System.out.println("axb: " + (a * b));
+    }
+}
