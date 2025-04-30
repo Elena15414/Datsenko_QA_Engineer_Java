19e09f112dff36391eb5389bf1b0b41c721372cf
Elena Datsenko <elenasheglova15414@gmail.com>
2025-04-30 23:33:01 +0300

    rename

--- src/main/java/ru/datsenko/Task1.java
+++ src/main/java/ru/datsenko/Task1.java
@@ -2,7 +2,7 @@ package ru.datsenko;
 
 import java.util.Scanner;
 
-public class Main {
+public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
