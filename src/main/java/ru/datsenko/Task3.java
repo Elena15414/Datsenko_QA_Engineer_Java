1fc1227c6944d0c528f81bf2db1c5832208aa01e
Elena Datsenko <elenasheglova15414@gmail.com>
2025-04-30 23:39:33 +0300

    gitignore

--- src/main/java/ru/datsenko/Task3.java
+++ src/main/java/ru/datsenko/Task3.java
@@ -1,2 +1,13 @@
-package ru.datsenko;public class Task3 {
+package ru.datsenko;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < 10; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
 }
