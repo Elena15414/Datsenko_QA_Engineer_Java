34237366a47e1c4c77222e64f25c6f10ca79c8ab
Elena Datsenko <elenasheglova15414@gmail.com>
2025-04-30 23:35:18 +0300

    task2

---
+++ src/main/java/ru/datsenko/Task2.java
@@ -0,0 +1,18 @@
+package ru.datsenko;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        sc.close();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+    }
+}
